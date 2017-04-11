caa3dab1aaf98fc341f3df85d2069bad2e210016
Gelios-Sky <jeliosfive@gmail.com>
2017-04-11 18:47:26 +0300

    Task_11

---
+++ Task_11.py
@@ -0,0 +1,10 @@
+def formula(c):
+    import math
+    return c*(math.pi/180)
+import math
+cos60=formula(math.cos(60))
+print(cos60)
+cos40=formula(math.cos(40))
+print(cos40)
+cos90=formula(math.cos(90))
+print(cos90)
