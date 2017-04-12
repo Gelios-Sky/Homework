0fe57abf82cf0bad8b659b9e43798d7d6e298abb
Gelios-Sky <jeliosfive@gmail.com>
2017-04-12 17:10:39 +0300

    Task_12 with remarks

--- Task_12.py
+++ Task_12.py
@@ -1,8 +1,8 @@
-def formula(number):
+def sum_numerals(number):
     numeral_1=number%10
     numeral_2=number%100//10
     numeral_3=number//100
     result=numeral_1+numeral_2+numeral_3
     return result
-#print(formula(454))
-number=int(input('Enter your number'))
+#sum_numerals=input(int('Enter your number'))
+print(sum_numerals(232))
